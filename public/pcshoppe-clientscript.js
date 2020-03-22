f3b9e5961a19e03f89c90e2cae8916f1dd40ba14
Samuel Despain <despa3@byui.edu>
2020-03-21 22:43:05 -0700

    Final touches till next week

--- public/pcshoppe-clientscript.js
+++ public/pcshoppe-clientscript.js
@@ -2,61 +2,61 @@ function loadData() {
     $.get("/getCaseList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#cases").append("<option>" + data[i].type + "</option>");
+            $("#cases").append("<option value='" + data[i].price + "'>" + data[i].type + " - $" + data[i].price + "</option>");
         }
     });
     $.get("/getCoolerList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#coolers").append("<option>" + data[i].type + "</option>");
+            $("#coolers").append("<option value='" + data[i].price + "'>" + data[i].type + " - $" + data[i].price + "</option>");
         }
     });
     $.get("/getCpuList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#cpus").append("<option>" + data[i].cores + "-core @ " + data[i].clock + " GHz</option>");
+            $("#cpus").append("<option value='" + data[i].price + "'>" + data[i].cores + "-core @ " + data[i].clock + " GHz - $" + data[i].price + "</option>");
         }
     });
     $.get("/getGpuList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#gpus").append("<option>" + data[i].ram + " GB, " + data[i].cores + "-core @ " + data[i].clock + " GHz</option>");
+            $("#gpus").append("<option value='" + data[i].price + "'>" + data[i].ram + " GB, " + data[i].cores + "-core @ " + data[i].clock + " GHz - $" + data[i].price + "</option>");
         }
     });
     $.get("/getMonitorList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#monitors").append("<option>" + data[i].resolution + " - " + data[i].refresh + " Hz</option>");
+            $("#monitors").append("<option value='" + data[i].price + "'>" + data[i].resolution + " - " + data[i].refresh + " Hz - $" + data[i].price + "</option>");
         }
     });
     $.get("/getMotherboardList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#motherboards").append("<option>" + data[i].type + "</option>");
+            $("#motherboards").append("<option value='" + data[i].price + "'>" + data[i].type + " - $" + data[i].price + "</option>");
         }
     });
     $.get("/getOsList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#os").append("<option>" + data[i].name + "</option>");
+            $("#os").append("<option value='" + data[i].price + "'>" + data[i].name + " - $" + data[i].price + "</option>");
         }
     });
     $.get("/getPsuList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#psus").append("<option>" + data[i].wattage + "W</option>");
+            $("#psus").append("<option value='" + data[i].price + "'>" + data[i].wattage + "W - $" + data[i].price + "</option>");
         }
     });
     $.get("/getRamList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#ram").append("<option>" + data[i].size + " GB</option>");
+            $("#ram").append("<option value='" + data[i].price + "'>" + data[i].size + " GB - $" + data[i].price + "</option>");
         }
     });
     $.get("/getStorageList", function(data) {
         //console.log("Back from server with: " + JSON.stringify(data));
         for (var i = 0; i < data.length; i++) {
-            $("#storage").append("<option>" + data[i].type + " " + data[i].size + " GB</option>");
+            $("#storage").append("<option value='" + data[i].price + "'>" + data[i].type + " " + data[i].size + " GB - $" + data[i].price + "</option>");
         }
     });
 
